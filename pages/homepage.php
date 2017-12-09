e94080787279f58301445e04016e5c63d65b9c59
Aakashkumar Bhagat <ab997@afsconnect2.njit.edu>
2017-12-09 18:13:28 -0500

    error and notfound

--- pages/homepage.php
+++ pages/homepage.php
@@ -4,11 +4,10 @@
 <head>
   <meta charset="utf-8">
  
-<title>Project</title>
+<title>Final Project</title>
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
 <meta name="description" content="Final Project">
 <meta name="author" content="SitePoint">
-
 
 
 <!--[if lt IE 9]>
@@ -21,23 +20,17 @@
 
 <center><h1><a href="index.php?page=accounts&action=all" class="text-dark bg-info" .text-white>Display Accounts</a></h1></center>
 <center><h1><a href="index.php?page=tasks&action=all" class="text-dark bg-info" .text-white>Display Tasks</a></h1></center>
-<form class="form-group" action="index.php?page=accounts&action=login" method="POST">
-<div class="form-row align-items-center">
-    <div class="col-auto">
-<div class="container-fluid">
+
+<form action="index.php?page=accounts&action=login" method="POST">
+
 <label><b>Username</b></label>
-<input class="form-control" type="text" placeholder="Enter Username" name="email" required>
+<input  type="text" placeholder="Enter Username" name="email" required>
       <label><b>Password</b></label>
-        <input class="form-control" type="password" placeholder="Enter Password" name="password" required>
-<div class="container-fluid">
+        <input  type="password" placeholder="Enter Password" name="password" required>
   <button type="submit" class="btn btn-dark" >Login</button>
-  </div>
  
-    </div>
-    </div>
-    </div>
-
-
+ 
+    
 
 </form>
 <center><h1><a href="index.php?page=accounts&action=register" class="text-dark bg-info" .text-white>Register</a></h1></center>
--- pages/register.php
+++ pages/register.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
 
-    <title>The HTML5 Herald</title>
+    <title>Registration</title>
     <meta name="description" content="The HTML5 Herald">
     <meta name="author" content="SitePoint">
 
@@ -18,15 +18,33 @@
 <body>
 
 
-<form action="index.php?page=accounts&action=register" method="post">
-    First name: <input type="text" name="fname"><br>
-    Last name: <input type="text" name="lname"><br>
-    Email: <input type="text" name="email"><br>
-    Phone: <input type="text" name="phone"><br>
-    Birthday: <input type="text" name="birthday"><br>
-    Gender: <input type="text" name="gender"><br>
-    Password: <input type="password" name="password"><br>
-    <input type="submit" value="Submit form">
+<form action="index.php?page=accounts&action=register" method="POST">
+   
+   <div class="container">
+        <label><b>Email</b></label>
+        <input type="text" placeholder="Enter Email" name="email" required>
+
+        <label><b> Name</b></label>
+        <input type="text" placeholder="Enter First Name" name="fname" required>
+
+        <label><b>Surname </b></label>
+        <input type="text" placeholder="Enter Last Name" name="lname" required>
+
+        <label><b>Mobile Number</b></label>
+        <input type="text" placeholder="Enter Phone number" name="phone" required>
+        <label><b>Date of Birth</b></label>
+        <input type="text" placeholder="Enter Birthday" name="bday" required>
+
+        <label><b>Gender</b></label>
+        <input type="text" placeholder="Enter Gender" name="gender" required>
+
+        <label><b>Password</b></label>
+        <input type="password" placeholder="Enter Password" name="password" required>
+        <button type="submit">Register</button>
+
+</div>
+    
+   
 </form>
 
 
