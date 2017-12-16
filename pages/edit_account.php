19af99e98de5469bec24344bfead6196d9abc23c
Aakashkumar Bhagat <ab997@afsconnect2.njit.edu>
2017-12-16 03:15:41 -0500

    added css and altered form location

--- pages/edit_account.php
+++ pages/edit_account.php
@@ -1,13 +1,19 @@
 <?php include 'header.php'; ?>
 
+<style>
+    body{
+        background: -moz-linear-gradient(45deg, rgba(242, 34, 50, 0.5), rgba(255, 187, 54, 0.6) 100%);
+        background: -webkit-linear-gradient(45deg, rgba(242, 34, 50, 0.5), rgba(255, 187, 54, 0.6) 100%);
+        background: linear-gradient(to 45deg, rgba(29, 236, 197, 0.4), rgba(96, 0, 136, 0.4) 100%);
+    }
+</style>
 
-
-        <?php $array = get_object_vars($data) ?>
+        <?php //$array = get_object_vars($data) ?>
 
         <?php $string='https://web.njit.edu/~ab997/FinalP/index.php?page=accounts&action=update' ?>
 
 
-
+<center>
 
         <form action=<?php echo $string ?> method="POST">
 
@@ -46,13 +52,13 @@
 
                 <label><b>password</b></label>
                 <input type="text" value="<?php echo $array['password'] ?>" name="password" >
+                <br>
 
                 <button type="submit">Update</button>
             </div>
 
-
+            </center>
 
         </form>
 
 
-  
